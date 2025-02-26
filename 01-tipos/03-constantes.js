f72178a5c44b7e10a412c5c75dc5172c9f2b3a26
ArturoFC1 <arthurcode420@gmail.com>
2025-02-26 16:33:54 -0600

    Estos cambios los he realizado de tal modo en lo que avanzo del curso de js, ya llevamos 1 hora.

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,5 @@
+const nombre = "Arturo";
+nombre = "FigueroaC";
+console.log(nombre);
+
+//En este codigo se nota cuando no se quiere cambiar el valor a una variable.
---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,12 @@
+let nombre = "Arturo";
+let numero = 23; //Esta line de codigo es para una linea de comentario
+
+/*
+Esta linea de comentario supuestamente es la oficial
+de tal modo 
+es la que 
+tenemos
+que
+usar
+*/
+console.log("Yo soy " + nombre + " y tengo "+ numero + " años"); 
