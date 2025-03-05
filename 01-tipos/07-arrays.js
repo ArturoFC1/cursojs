5316a0bbc81d98b6f8ad6fb918c0c0ace4720866
ArturoFC1 <arthurcode420@gmail.com>
2025-03-05 16:15:48 -0600

    Este cambio de realizo para tomar la clase de Arreglos de JS

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,15 @@
+//Creacion de un arreglo
+let arreglo = ['bmw','cupra','mercedez','italika'];
+console.log(arreglo)
+
+//Impresion de un objeto del arreglo
+console.log(arreglo[3]);
+
+//Agregar objeto al arreglo
+
+arreglo[4] = 'audi';
+arreglo[5] = 'tesla';
+console.log(arreglo);
+
+//Ver tamaño total del arreglo
+console.log("El tamaño del arreglo es: "+ arreglo.length);
